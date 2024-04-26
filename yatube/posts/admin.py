5f963f02583bf753a40d4d04cd25ddcb16e3e3b1
svshatunova <svshatun0va@yandex.ru>
2024-04-26 22:44:37 +0300

    fix

--- yatube/posts/admin.py
+++ yatube/posts/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from .models import Post, Group
 
+
 class PostAdmin(admin.ModelAdmin):
     list_display = (
         'pk',
@@ -14,6 +15,7 @@ class PostAdmin(admin.ModelAdmin):
     list_filter = ('pub_date',)
 
     empty_value_display = '-пусто-'
+
 
 admin.site.register(Post, PostAdmin)
 admin.site.register(Group)
